e10d8397b75589598b61d0820f78dc6517ac93e9
Niels van Galen Last <nvangalenlast@gmail.com>
2018-02-17 14:55:50 +0100

    :sparkles: feat(core): change parser to parse emoji from commit message

--- conventional-recommended-bump.js
+++ conventional-recommended-bump.js
@@ -27,7 +27,8 @@ module.exports = {
 	},
 
 	parserOpts: {
-		headerPattern: /^(\w*)(?:\((.*)\))?\: (.*)$/,
+		// headerPattern: /^(\w*)(?:\((.*)\))?\: (.*)$/,
+		headerPattern: /^(\w*|\:\w*\: \w*)(?:\((.*)\))?\: (.*)$/,
 		headerCorrespondence: [`type`, `scope`, `subject`],
 		noteKeywords: `BREAKING CHANGE`,
 		revertPattern: /^revert:\s([\s\S]*?)\s*This reverts commit (\w*)\./,
